437ab20512d7551b5f0bf3bcdda8e606f03b9fe4
JerrodBolton <rodrod1@JerrodBltonsMBP.hsd1.tn.comcast.net>
2021-07-31 17:05:11 -0500

    added 7th question

--- aboutme.js
+++ aboutme.js
@@ -1,43 +1,43 @@
-// I want the ues to input their name and they will get a welcome form th website.
+//I want the ues to input their name and they will get a welcome form th website.
 
-function inNames(){
-let usernames = prompt("What is your name?");
- document.write(" Hey, welcome " + usernames + " happy for here you here!");
+function inNames() {
+  let usernames = prompt("What is your name?");
+  document.write(" Hey, welcome " + usernames + " happy for here you here!");
 }
-  "use strict";
 
-function quiz() {
-  // if the x don't put Jerrod Bolton then it's going to put you wrong.
+("use strict");
+var totalScore = "0";
 
-  let name = true;
-
-  while (name) {
-    let response = prompt(" Is my name Jerrod Bolton?");
-    if (response.toUpperCase() === "YES" || response.toUpperCase() === "Y"){
-      name = false;
+function question1() {
+  let myName = true;
+  while (myName) {
+    let response = prompt("Is my name Jerrod Bolton?");
+    if (response.toUpperCase() === "YES" || response.toUpperCase() === "Y") {
+      myName = false;
       alert("Your Correct!!!!");
       console.log(response);
-    } 
-    else {
+      totalScore++;
+    } else {
       alert("Your answer is incorrect");
     }
   }
-
+}
+function question2() {
   let school = true;
 
   while (school) {
     let response2 = prompt("Did I go to freedom Prep?");
-    if (response2.toUpperCase() === "YES" || response2.toUpperCase() === "Y" ) {
+    if (response2.toUpperCase() === "YES" || response2.toUpperCase() === "Y") {
       school = false;
       alert("Your Correct!!!!");
       console.log(response2);
-     
-    } 
-    else {
+      totalScore++;
+    } else {
       alert("Your answer is incorrect");
     }
   }
-
+}
+function question3() {
   let graduate = true;
 
   while (graduate) {
@@ -46,81 +46,133 @@ function quiz() {
       graduate = false;
       alert("Your Correct!!!!");
       console.log(response3);
-
-    } 
-    else {
+      totalScore++;
+    } else {
       alert("Your answer is incorrect");
     }
   }
-
+}
+function question4() {
   let college = true;
 
-  while (college){
+  while (college) {
     let response5 = prompt(" Was college easy for me?");
-    if (response5.toUpperCase() === "NO" || response5.toUpperCase() === "N" ) {
+    if (response5.toUpperCase() === "NO" || response5.toUpperCase() === "N") {
       college = false;
       alert("Your Correct!!!!");
       console.log(response5);
-        }   
-         else{
+      totalScore++;
+    } else {
       alert("Your answer is incorrect");
-    }
-   } 
-  
-
-  let golds = true;
-
-  while (golds) {
-    let response4 = prompt("Is my gold to be a software engineer?");
-    if (response4.toUpperCase() === "YES" || response4.toUpperCase() === "Y" ) {
-       golds = false;
-      alert("Your Correct!!!!");
-      console.log(response4);
-    }
-     else {
-      alert("Your answer is incorrect");
-
     }
   }
 }
 
- function guessgamemagicNunber(){
-let magicNumber = "15";
+function question5() {
+  let golds = true;
 
-alert (" You four chances to guess the correct number. 1-20");
-
-  for(let x = 0 ; x < 4; x++){
-
-    let guess = prompt("What is the magic number 1-20?");
-
-   // guess is greatest the magicNumber
-
-    if(guess > magicNumber ){
-        
-       alert("Your to high");
-      
+  while (golds) {
+    let response4 = prompt("Is my gold to be a software engineer?");
+    if (response4.toUpperCase() === "YES" || response4.toUpperCase() === "Y") {
+      golds = false;
+      alert("Your Correct!!!!");
+      console.log(response4);
+      totalScore++;
+    } else {
+      alert("Your answer is incorrect");
     }
-     
-     // guess is to low than magicNumber 
+  }
+}
+function question6() {
+  let career = true;
 
-     if( guess < magicNumber){
-       alert("Your to low");
-     }
+  while (career) {
+    let response8 = prompt("Am I working for a better career");
+    if (response8.toUpperCase() === "YES" || response8.toUpperCase() === "Y") {
+      career = false;
+      alert("Your Correct!!!!");
+      console.log(response8);
+      totalScore--;
+    } else {
+      alert("Your answer is incorrect");
+    }
+  }
+}
 
-     // guess is so close
-     if (guess === "14" ){
-       alert("So close but wrong to low!!!");
-     }
-     if(guess === magicNumber){
-       alert( "You got it");
-    
-      break;
-     }
-   }
-   alert ("The correct answer is 15");
- }
- guessgamemagicNunber();
+function sleepQuestion() {
+  let sleepHours = ["5", "6", "7", "8", "12"];
 
-// let guessGame = ["May","13","2000"]
+  for (let j = 1; j <= 6; j++) {
+    // if guessGame is incorrect 6 time than you will know what is the correct answer.
+    let numberOfHS = prompt(
+      "With the amount of work I do; how much sleep do you think that I get?"
+    ); // this is where I ask the question.
 
-//  saidAnswer= prompt("What is my birthday")
+    let correctAnswer = false;
+    for (i = 0; i < sleepHours.length; i++) {
+      if (numberOfHS === sleepHours[i]) {
+        // If the user answer correctly, the first 'for' loop will run again
+        totalScore++;
+        alert("That's right! Guess again?");
+        correctAnswer = true;
+
+        break;
+      }
+    }
+    if (correctAnswer === false) {
+      alert("I'm not crazy");
+      // this is when someone get the wrong answer.
+    }
+  }
+  alert(
+    "Thanks for playing! the correct answers were " + sleepHours + " Enjoy!!");
+}
+function Score() {
+  alert("Score total is " + totalScore);
+}
+
+function quiz() {
+  question1();
+  question2();
+  question4();
+  question3();
+  question5();
+  question6();
+  sleepQuestion();
+  Score();
+}
+
+// function guessGameMagicNumber() {
+//   let magicNumber = "15";
+
+//   alert(" You four chances to guess the correct number. 1-20");
+
+//   for (let x = 0; x < 4; x++) {
+//     let guess = prompt("What is the magic number 1-20?");
+
+//     // guess is greatest the magicNumber
+
+//     if (guess > magicNumber) {
+//       alert("Your to high");
+//     }
+
+//     // guess is to low than magicNumber
+
+//     if (guess < magicNumber) {
+//       alert("Your to low");
+//     }
+
+//     // guess is so close
+//     if (guess === "14") {
+//       alert("So close but wrong to low!!!");
+//     }
+//     if (guess === magicNumber) {
+//       alert("You got it");
+
+//       break;
+//     }
+//   }
+//   alert("The correct answer is 15");
+// }
+
+// guessGameMagicNumber();
